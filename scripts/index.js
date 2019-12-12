a56757cf677203f3475c80172ade6cf9b6ffe3d1
Bentley Herron <10934614+bentleyherron@users.noreply.github.com>
2019-12-12 14:02:47 -0500

    Updated stuff. I forget what

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,11 @@
-// Set temp variables
-const originAirport = `ATL`;
+// Set Global variables
+let originAirport = `ATL`;
+let destAirport = '';
+let randAirportNum = '';
+let todayDate = '';
+let skyscannerServer = '';
+const geoIPServer = `https://api.ipdata.co/?api-key=${ipLookupAPI}`;
+let userIP = '';
 
 // Get random number to select destination airport from object (airportCodes) and dave to randAirportNum variable
 function getRandomInt(min, max) {
@@ -7,7 +13,6 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-const randAirportNum = getRandomInt(0, 50);
 
 // Get todays date and save to todayDate variable
 function getTodayDate() {
@@ -17,25 +22,51 @@ function getTodayDate() {
     var yyyy = today.getFullYear();
     return yyyy + '-' + mm + '-' + dd
 }
-const todayDate = getTodayDate();
 
 // Check user IP 
-const geoIPServer = `https://api.ipdata.co/?api-key=${ipLookupAPI}`;
-const userIP = '';
 function getUserIP() {
     fetch(geoIPServer)
         .then(r => r.json())
         .then(obj => {
-            const userIP = obj.ip;
+            userIP = obj.ip;
         })
     return userIP;
 }
 
 // Uses randAirportNum to get airport from airportCodes object
-destAirport = airportCodes[randAirportNum]['iata'];
+function getRandomAirport() {
+    randAirportNum = getRandomInt(0, 50);
+    destAirport = airportCodes[randAirportNum]['iata'];
+    skyscannerServer = `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/${originAirport}-sky/${destAirport}-sky/${todayDate}`;
 
-// Sets variable for request server with variable information
-const skyscannerServer = `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/${originAirport}-sky/${destAirport}-sky/${todayDate}`;
+}
+
+// Shows Information for flight quotes. If no quotes avaliable, check another airport
+function checkForQuotes(obj) {
+    if (obj['Quotes'].length === 0) {
+        console.log('No flights avaliable. Getting new airport.')
+        getRandomAirport();
+        getQuotes();
+    } else if (obj['Quotes'].length > 0) {
+        showFlightQuotes(obj);
+    } else {
+        console.log('Unknown Error.')
+    }
+
+}
+
+function showFlightQuotes(obj) {
+    // console.log(obj);
+    console.log(`From: ${obj['Places']['0']['CityName']}`)
+    console.log(`To: ${obj['Places']['1']['CityName']}`)
+    for (quote of obj['Quotes']) {
+        // console.log(quote);
+        console.log(`Minimum Price: $${quote['MinPrice']}`);
+        const airlineNumber = quote['OutboundLeg']['CarrierIds']['0'];
+        // console.log(`Airline: $${obj['Carriers'][airlineNumber]['Name']}`);
+        console.log(`Direct Flight?: ${quote['Direct']}`);
+    }
+}
 
 // Gets quotes from server and console.logs the object
 function getQuotes() {
@@ -45,12 +76,14 @@ function getQuotes() {
           },
     })
         .then(r => r.json())
-        .then(obj => {
-            console.log(obj);       // Console logs server response for testing
-            // console.log(obj['Quotes']);
-            // console.log(obj['Quotes']["0"]['MinPrice']);
-        })
+        .then(checkForQuotes)
+}
+
+function main() {
+    todayDate = getTodayDate();
+    getRandomAirport();
+    getQuotes();
 }
 
 // Run
-getQuotes();
+main();
