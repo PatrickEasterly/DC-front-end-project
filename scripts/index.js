8fda956af5fbaf4f73c9b0ac727564bfd9dc027d
Bentley Herron <10934614+bentleyherron@users.noreply.github.com>
2019-12-12 11:09:24 -0500

    Added comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,13 @@
+// Sets variables for query
 const originAirport = `SFO`;
 const destAirport = `JFK`;
 const date = `2019-12-24`;
 
+// Request server
 const skyscannerServer = `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/${originAirport}-sky/${destAirport}-sky/${date}`;
 
 
+// Gets quotes from server and console.logs the object
 function getQuotes() {
     fetch(skyscannerServer, {
         headers: {
@@ -13,8 +16,9 @@ function getQuotes() {
     })
         .then(r => r.json())
         .then(out => {
-            console.log(out);
+            console.log(out);       // Console logs server response for testing
         })
 }
 
+// Run
 getQuotes();
